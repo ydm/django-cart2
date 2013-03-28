a581408158f53d572de8c349090bd5b470adc778
Yordan Miladinov <yordan@4web.bg>
2013-03-29 01:59:47 +0200

    changes towards compatibility with python3

--- cart/models.py
+++ cart/models.py
@@ -1,10 +1,19 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0232
+# W0232: Class has no __init__ method
+
+from __future__ import unicode_literals
+
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
 
 class Cart(models.Model):
-    creation_date = models.DateTimeField(verbose_name=_('creation date'))
+    creation_date = models.DateTimeField(auto_now_add=True,
+                                         verbose_name=_('creation date'))
     checked_out = models.BooleanField(default=False, verbose_name=_('checked out'))
 
     class Meta:
@@ -12,8 +21,10 @@ class Cart(models.Model):
         verbose_name_plural = _('carts')
         ordering = ('-creation_date',)
 
-    def __unicode__(self):
+    @python_2_unicode_compatible
+    def __str__(self):
         return unicode(self.creation_date)
+
 
 class ItemManager(models.Manager):
     def get(self, *args, **kwargs):
@@ -22,6 +33,7 @@ class ItemManager(models.Manager):
             kwargs['object_id'] = kwargs['product'].pk
             del(kwargs['product'])
         return super(ItemManager, self).get(*args, **kwargs)
+
 
 class Item(models.Model):
     cart = models.ForeignKey(Cart, verbose_name=_('cart'))
@@ -38,20 +50,19 @@ class Item(models.Model):
         verbose_name_plural = _('items')
         ordering = ('cart',)
 
-    def __unicode__(self):
+    @python_2_unicode_compatible
+    def __str__(self):
         return u'%d units of %s' % (self.quantity, self.product.__class__.__name__)
 
+    @property
     def total_price(self):
         return self.quantity * self.unit_price
-    total_price = property(total_price)
 
-    # product
-    def get_product(self):
+    @property
+    def product(self):
         return self.content_type.get_object_for_this_type(id=self.object_id)
 
-    def set_product(self, product):
+    @product.setter
+    def product(self, product):
         self.content_type = ContentType.objects.get_for_model(type(product))
         self.object_id = product.pk
-
-    product = property(get_product, set_product)
-
