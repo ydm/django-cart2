7dc71da45c339bac30987707297e3ebc9d7696f5
Yordan Miladinov <yordan@4web.bg>
2013-03-29 06:32:09 +0200

    simplify models; optimize performance, replace arhaic calls with new ones (this drops the 1.1 support), improve code readability

--- cart/__init__.py
+++ cart/__init__.py
@@ -1,96 +1,52 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import datetime
 
 from cart import models
 
 
-class CartException(Exception):
-    pass
-
-
-class ItemAlreadyExists(CartException):
-    pass
-
-
-class ItemDoesNotExist(CartException):
-    pass
-
-
 class Cart:
+
     def __init__(self, request):
-        cart_id = request.session.get('CART_ID')
-        if cart_id:
+        cart = None
+        pk = request.session.get('__cart__')
+        if pk:
             try:
-                cart = models.Cart.objects.get(id=cart_id, checked_out=False)
+                cart = models.Cart.objects.get(pk=pk)
             except models.Cart.DoesNotExist:
-                cart = self.new(request)
-        else:
-            cart = self.new(request)
+                pass
+        if not cart:
+            cart = models.Cart()
+            cart.save()
+            request.session['__cart__'] = cart.pk
         self.cart = cart
 
     def __iter__(self):
-        for item in self.cart.item_set.all():
+        for item in self.cart.item_set.all().iterator():
             yield item
 
-    def new(self, request):
-        cart = models.Cart(creation_date=datetime.datetime.now())
-        cart.save()
-        request.session['CART_ID'] = cart.id
-        return cart
-
-    def add(self, product, unit_price, quantity=1):
-        try:
-            item = models.Item.objects.get(
-                cart=self.cart,
-                product=product,
-            )
-        except models.Item.DoesNotExist:
-            item = models.Item()
-            item.cart = self.cart
-            item.product = product
-            item.unit_price = unit_price
+    def add(self, product, quantity=1):
+        defaults = {'quantity': quantity}
+        item, created = models.Item.objects.get_or_created(
+            cart=self.cart, product=product, defaults=defaults)
+        if not created:
             item.quantity = quantity
             item.save()
-        else: #ItemAlreadyExists
-            item.unit_price = unit_price
-            item.quantity = item.quantity + int(quantity)
-            item.save()
+
+    def incrby_quantity(self, product, value):
+        """Add integer `value` to item.quantity atomically"""
+        item = models.Item.objects.select_for_update().get(
+            cart=self.cart, product=product)
+        item.quantity += int(value)
+        item.save()
 
     def remove(self, product):
-        try:
-            item = models.Item.objects.get(
-                cart=self.cart,
-                product=product,
-            )
-        except models.Item.DoesNotExist:
-            raise ItemDoesNotExist
-        else:
-            item.delete()
+        item = models.Item.objects.get(cart=self.cart, product=product)
+        item.delete()
 
-    # TODO
-    def update(self, product, quantity, unit_price=None):
-        try:
-            item = models.Item.objects.get(
-                cart=self.cart,
-                product=product,
-            )
-        except models.Item.DoesNotExist:
-            raise ItemDoesNotExist
-            
     def count(self):
-        result = 0
-        for item in self.cart.item_set.all():
-            result += 1 * item.quantity
-        return result
+        return sum(item.quantity for item in self.cart.item_set.all())
         
-    def summary(self):
-        result = 0
-        for item in self.cart.item_set.all():
-            result += item.total_price
-        return result
-
-    def clear(self):
+    def remove_all(self):
         for item in self.cart.item_set.all():
             item.delete()
--- cart/models.py
+++ cart/models.py
@@ -6,63 +6,44 @@
 from __future__ import unicode_literals
 
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
 
 
 class Cart(models.Model):
-    creation_date = models.DateTimeField(auto_now_add=True,
-                                         verbose_name=_('creation date'))
-    checked_out = models.BooleanField(default=False, verbose_name=_('checked out'))
-
-    class Meta:
-        verbose_name = _('cart')
-        verbose_name_plural = _('carts')
-        ordering = ('-creation_date',)
+    created = models.DateTimeField(auto_now_add=True)
+    checked_out = models.BooleanField(default=False)
 
     @python_2_unicode_compatible
     def __str__(self):
-        return unicode(self.creation_date)
-
-
-class ItemManager(models.Manager):
-    def get(self, *args, **kwargs):
-        if 'product' in kwargs:
-            kwargs['content_type'] = ContentType.objects.get_for_model(type(kwargs['product']))
-            kwargs['object_id'] = kwargs['product'].pk
-            del(kwargs['product'])
-        return super(ItemManager, self).get(*args, **kwargs)
+        return '{}, {}'.format(self.pk, self.created)
 
 
 class Item(models.Model):
-    cart = models.ForeignKey(Cart, verbose_name=_('cart'))
-    quantity = models.PositiveIntegerField(verbose_name=_('quantity'))
-    unit_price = models.DecimalField(max_digits=18, decimal_places=2, verbose_name=_('unit price'))
-    # product as generic relation
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
 
-    objects = ItemManager()
+    cart = models.ForeignKey(Cart)
+    quantity = models.PositiveIntegerField()
+
+    product_type = models.ForeignKey(ContentType)
+    product_id = models.PositiveIntegerField()
+    _product = generic.GenericForeignKey('product_type', 'product_id')
 
     class Meta:
-        verbose_name = _('item')
-        verbose_name_plural = _('items')
+        verbose_name = 'item'
+        verbose_name_plural = 'items'
         ordering = ('cart',)
+        unique_together = ('cart', 'product_type', 'product_id')
 
     @python_2_unicode_compatible
     def __str__(self):
         return u'%d units of %s' % (self.quantity, self.product.__class__.__name__)
 
     @property
-    def total_price(self):
-        return self.quantity * self.unit_price
-
-    @property
     def product(self):
-        return self.content_type.get_object_for_this_type(id=self.object_id)
+        return self._product
 
     @product.setter
-    def product(self, product):
-        self.content_type = ContentType.objects.get_for_model(type(product))
-        self.object_id = product.pk
+    def product(self, value):
+        self.product_type = ContentType.objects.get_for_model(value)
+        self.product_id = value.pk
