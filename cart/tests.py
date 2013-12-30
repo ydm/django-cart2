03c8b371ba880a66b4237d32b77cb897c1afa9c3
Yordan Miladinov <yordanm@elektrosfera.eu>
2013-12-30 14:36:07 +0200

    y: Write some tests to assert the meta information stuff works correctly

--- cart/tests.py
+++ cart/tests.py
@@ -9,11 +9,14 @@ from django.test import client
 
 import cart
 from cart import models
+from cart.meta import serialize
 
 
 class CartTest(TestCase):
 
     def _cart(self):
+        """Return always a fresh cart"""
+        cart.cleanup(self._request().session)
         return cart.Cart(self._request().session)
 
     def _product(self, num):
@@ -43,3 +46,46 @@ class CartTest(TestCase):
         # are actually removed
         c.remove_different(part)
         self.assertEqual(part, c._products(False))
+
+    def test_add_meta(self):
+        c = self._cart()
+
+        # Add some products with meta information attached to them
+        for i in range(10):
+            c.add(self._product(i), meta={'i': i})
+
+        # Check if they're all stored correctly
+        for i, item in enumerate(c):
+            self.assertEqual(i, item.meta['i'])
+
+        # Change all product metas and assert with a separate query that
+        # it's correctly stored into the databse
+        for i, item in enumerate(c):
+            item.meta['i'] = i * 2
+
+        # Check if everything's correct
+        for i, item in enumerate(c):
+            self.assertEqual(i * 2, item.meta['i'])
+
+    def test_meta_db(self):
+        c = self._cart()
+        for i in range(10):
+            p = self._product(i)
+            c.add(p, meta={'i': i})
+
+            # Run a manual query to assert everything in the database is
+            # correctly stored
+            sql = 'SELECT * FROM cart_item WHERE id={}'.format(p.pk)
+
+            rawitem = models.TestProduct.objects.raw(sql)[0]
+            expected = serialize({'i': i})
+            actual = rawitem.metafld
+            self.assertEqual(expected, actual)
+
+            # Now change the meta value and rerun the same query
+            c.set_meta(p, 'i', i*2)
+
+            rawitem2 = models.TestProduct.objects.raw(sql)[0]
+            expected2 = serialize({'i': i*2})
+            actual2 = rawitem2.metafld
+            self.assertEqual(expected2, actual2)
